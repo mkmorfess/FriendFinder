503b6bad383271fbc89f2377bf6cc733a6a4ebb5
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-12-12 10:22:47 -0600

    Dynamically added the 10 questions dropdown

--- assets/survey.js
+++ assets/survey.js
@@ -1,6 +1,46 @@
 $(document).ready(function(){
+	var idNum = 1;
+// $(":selected")
 
-$(":selected")
+	for (var i = 0; i < 11; i++) {
 
+		var newSelect = $("<select>");
+
+		newSelect.attr("id", "q" + idNum);
+
+		$("#chooseSelect-" + idNum).html(newSelect);
+
+		for (var j = 0; j < 6; j++) {
+
+			if (j === 0) {
+				var options = $("<option>");
+				options.text("Please Choose A Number");
+				newSelect.append(options);
+			}
+			else if (j === 1) {
+				var options = $("<option>");
+				options.attr("value", j);
+				options.text(j + " - (Strongly Disagree)");
+				newSelect.append(options);
+			}
+
+			else if (j === 5) {
+				var options = $("<option>");
+				options.attr("value", j);
+				options.text(j + " - (Strongly Agree)");
+				newSelect.append(options);
+			}
+
+			else{
+
+				var options = $("<option>");
+				options.attr("value", j);
+				options.text(j);
+				newSelect.append(options);
+			}
+			
+		}
+		idNum++
+	}
 
 })
