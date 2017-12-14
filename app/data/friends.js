ab5f03231bc13be5089769b266e0f61b3b7899ac
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-12-14 11:37:21 -0600

    Updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 	// $(":selected")
 	var matches = [];
-	//{NewFriend: {name: "Mike", photo: "picture", scores:["1", "3", "4", "5", "2", "3", "5", "1", "2", "5"]}}
-	// console.log(matches);
+	
 
 	function NewFriend (name, photo, scores) {
 
@@ -10,14 +9,7 @@ $(document).ready(function(){
 		this.photo = photo;
 		this.scores = scores;
 
-		// this.checkBestMatch = function(){
-
-		// 	for (var i = 0; i < matches.length; i++) {
-		// 		for (var j = 0; j < matches[i].scores.length; j++) {
-		// 			var userAdd += Math.abs(currentEntry.scores[j] - matches[i].scores[j]);
-		// 		}
-		// 	}
-		// }
+		
 
 	}
 
@@ -143,9 +135,11 @@ $(document).ready(function(){
 
 					}
 
+					//https://forums.adobe.com/thread/1062126 -- Nice bubble sort code
+
 					bubbleSort(scores,names);  
 
-					  function bubbleSort(a,b)  {  
+					function bubbleSort(a,b)  {  
 					    var swapped;  
 					    do {  
 					        swapped = false;  
@@ -202,109 +196,10 @@ $(document).ready(function(){
 					}
 
 
-
-					// var currentName;
-					// var otherName;
-					// var compareName1;
-					// var compareName2;
-
-					// var nameArray = [];
-
-					// // var potentialMatches = [];
-
-					// var userCompare1 = [];
-					// var userCompare2 = [];
-
-					// var add1 = 0;
-					// var add2 = 0;				
-
-					// 	var currentUser = currentEntry.scores
-
-					// 	for (var j = 0; j < matches.length - 1; j++) {
-
-					// 		var otherUsers = matches[j];
-					// 		currentName = currentEntry.name
-					// 		otherName = matches[j].name;
-					// 		console.log(otherName);
-					// 		console.log(currentName);
-
-							
-
-					// 			if (userCompare1.length === 0) {
-
-					// 				for (var i = 0; i < 10; i++) {
-					// 					userCompare1.push(Math.abs(currentUser[i] - otherUsers.scores[i]));
-					// 					compareName1 = otherName;
-
-										
-					// 				}
-
-								
-					// 				console.log(userCompare1);
-					// 				console.log(compareName1);
-									
-					// 			}
-
-					// 			else {
-					// 				for (var i = 0; i < 10; i++) {
-					// 					userCompare2.push(Math.abs(currentUser[i] - otherUsers.scores[i]));
-					// 					compareName2 = otherName;
-										
-					// 				}
-					// 				console.log(userCompare2);
-					// 				console.log(compareName2);
-
-					// 				for (var i = 0; i < 10; i++) {
-					// 					add1 += userCompare1[i];
-					// 					add2 += userCompare2[i];
-					// 				}
-
-					// 				console.log(add1);
-					// 				console.log(add2);
-
-					// 				if (add1 > add2) {
-
-					// 					userCompare1 = [];
-
-					// 					for (var i = 0; i < 10; i++) {
-											
-					// 						userCompare1.push(userCompare2[i]);
-					// 						compareName1 = compareName2;
-					// 					}
-					// 					userCompare2 = [];
-					// 					console.log(userCompare1);
-					// 					console.log(compareName1);
-										
-					// 				}
-
-					// 				if (add2 > add1) {
-					// 					userCompare2 = [];
-					// 					compareName2 = ""
-					// 				}
-
-									
-
-					// 				if (add1 === add2) {
-
-					// 					// nameArray.push(compareName1);
-					// 					// nameArray.push(compareName2);
-
-					// 				}
-
-					// 				add1 = 0;
-					// 				add2 = 0;
-
-					// 			}
-
-
-					// 		}
-
-							
-					// 		alert("Your match is... " + compareName1);
-						}
+				}
 
 					
-				}
+			}
 
 			
 
