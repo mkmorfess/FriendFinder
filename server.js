59dcd09a509ce62519957ecb91aad90f3adb9fb0
Michael Morfessis <mkmorfess@yahoo.com>
2017-12-19 19:47:17 -0600

    Fixed a slight bug with the catchall function

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'app/public')))
 app.use('/survey/', express.static(path.join(__dirname, 'app/public')))
-app.use('*', express.static(path.join(__dirname, 'app/public')))
+app.use(express.static(path.join(__dirname, 'app/public')))
 // ...
 // ...
 htmlRoutes.appHome(app, path);
